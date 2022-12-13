609f8f9ccb00529702bad0c3798a7d8bb766e6ec
rasoul707 <r.ahmadifar.1377@gmail.com>
2022-12-13 11:37:18 +0330

    check  email

--- src/Services/Mail/Smtp.php
+++ src/Services/Mail/Smtp.php
@@ -13,22 +13,22 @@ class Smtp extends Base
 
     public function __construct()
     {
-        $this->config = $this->getConfig();
-        $mail = new PHPMailer();
-        //$mail->SMTPDebug = 3;                               // Enable verbose debug output
-        $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = $this->config['host'];  // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = $this->config['username'];                 // SMTP username
-        $mail->Password = $this->config['passsword'];                    // SMTP password
-        if ($_ENV['smtp_ssl']) {
-            $mail->SMTPSecure = ($_ENV['smtp_port'] == 587 ? 'tls' : 'ssl');                            // Enable TLS encryption, `ssl` also accepted
-        }
-        $mail->Port = $this->config['port'];                                    // TCP port to connect to
-        $mail->setFrom($this->config['sender'], $this->config['name']);
-        $mail->addReplyTo($this->config['reply_to'], $this->config['reply_to_name']);
-        $mail->CharSet = 'UTF-8';
-        $this->mail = $mail;
+        // $this->config = $this->getConfig();
+        // $mail = new PHPMailer();
+        // //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+        // $mail->isSMTP();                                      // Set mailer to use SMTP
+        // $mail->Host = $this->config['host'];  // Specify main and backup SMTP servers
+        // $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        // $mail->Username = $this->config['username'];                 // SMTP username
+        // $mail->Password = $this->config['passsword'];                    // SMTP password
+        // if ($_ENV['smtp_ssl']) {
+        //     $mail->SMTPSecure = ($_ENV['smtp_port'] == 587 ? 'tls' : 'ssl');                            // Enable TLS encryption, `ssl` also accepted
+        // }
+        // $mail->Port = $this->config['port'];                                    // TCP port to connect to
+        // $mail->setFrom($this->config['sender'], $this->config['name']);
+        // $mail->addReplyTo($this->config['reply_to'], $this->config['reply_to_name']);
+        // $mail->CharSet = 'UTF-8';
+        // $this->mail = $mail;
     }
 
     public function getConfig()
@@ -47,14 +47,15 @@ class Smtp extends Base
 
     public function send($to, $subject, $text, $files)
     {
-        $mail = $this->mail;
-        $mail->addAddress($to);     // Add a recipient
-        $mail->isHTML();
-        $mail->Subject = $subject;
-        $mail->Body = $text;
-        foreach ($files as $file) {
-            $mail->addAttachment($file);
-        }
+        $mail = new PHPMailer();
+        // $mail = $this->mail;
+        // $mail->addAddress($to);     // Add a recipient
+        // $mail->isHTML();
+        // $mail->Subject = $subject;
+        // $mail->Body = $text;
+        // foreach ($files as $file) {
+        //     $mail->addAttachment($file);
+        // }
         // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
         // if ($mail->send()) {
         //     return "gggg";
