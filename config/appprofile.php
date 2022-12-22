414788a3698ce06287177d5c7f83cd6ed38fcd43
rasoul707 <r.ahmadifar.1377@gmail.com>
2022-12-23 02:14:12 +0330

    surfboard profile and test payment

--- config/appprofile.php
+++ config/appprofile.php
@@ -361,8 +361,8 @@ $_ENV['Surfboard_Profiles'] = [
             'loglevel' => 'notify',
             'dns-server' => 'system, 8.8.8.8, 8.8.4.4',
             'skip-proxy' => '127.0.0.1, 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, 100.64.0.0/10, 17.0.0.0/8, localhost, *.local, *. crashlytics.com',
-            'internet-test-url' => 'https://www.google.com/',
-            'proxy-test-url' => 'https://www.google.com/',
+            'internet-test-url' => 'http://www.google.com/',
+            'proxy-test-url' => 'http://www.google.com/',
         ],
         'Proxy' => [
             '🎃 Nothing = direct',
