97293528e36afd73c2bfa40a3bcf953e0043ea9f
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-26 02:16:42 +0330

    fixss url

--- src/Controllers/MetronController.php
+++ src/Controllers/MetronController.php
@@ -499,6 +499,7 @@ class MetronController extends BaseController
                     # 不是多端口节点
                     $mu_port = Node::where('sort', '=', 9)->first();
                     $mu_user = User::where('port', '=', $mu_port->server)->value('port');
+                    $hhh = [$mu_port, $mu_user];
                     if (URL::SSRCanConnect($user, $mu_user)) {
                         $nodeinfo = $node->getItem($user, $mu_user, 0, 0);
                         $url = URL::getItemUrl($nodeinfo, 0);
@@ -523,6 +524,7 @@ class MetronController extends BaseController
                 }
                 $res = [
                     'ret' => 1,
+                    'hhhh' => $hhh,
                     'sort' => (int)$node->sort,
                     'info' => $nodeinfo,
                     'mu_user' => $mu_user,
