755f2c80a9dac52d0d17e3d1d66e72df293ed5aa
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-26 15:47:11 +0330

    FIX SSR

--- src/Controllers/MetronController.php
+++ src/Controllers/MetronController.php
@@ -497,9 +497,8 @@ class MetronController extends BaseController
             case '10':
                 if ($node->mu_only != -1) {
                     # 不是多端口节点
-                    // $mu_port = Node::where('sort', '=', 9)->first();
-                    // $mu_user = User::where('port', '=', $mu_port->server)->value('port');
-                    $mu_user = 10030;
+                    $mu_port = Node::where('sort', '=', 9)->first();
+                    $mu_user = User::where('port', '=', $mu_port->server)->value('port');
                     if (URL::SSRCanConnect($user, $mu_user)) {
                         $nodeinfo = $node->getItem($user, $mu_user, 0, 0);
                         $url = URL::getItemUrl($nodeinfo, 0);
